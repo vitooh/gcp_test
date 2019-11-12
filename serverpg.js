8470143498095fcb7137585c8e9b3267547a7350
vitooh <49574943+vitooh@users.noreply.github.com>
2019-11-12 23:21:47 +0100

    update with api

--- serverpg.js
+++ serverpg.js
@@ -3,10 +3,12 @@ var bodyParser = require('body-parser')
 var app = express()
 var port = 80
 var db = require('./queries')
+var api = require('./api')
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/test', db.getTest)
+app.get('/alltour', api.getAll)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

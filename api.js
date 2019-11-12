45d633303f8c3ddc72d4dafdaa028048e80883d2
vitooh <49574943+vitooh@users.noreply.github.com>
2019-11-13 00:14:03 +0100

    full api 1.0

--- api.js
+++ api.js
@@ -1,2 +1,18 @@
-var getAll =  (req, res) => res.send('lista z api')
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var getAll =  (req, res) => {
+        result = ""
+        for (var i = 0; i < 2000; i++) {
+                result = result + i + "   :   "  + httpGet("http://api.snooker.org/?e="+i)
+                result = result + "\n" 
+        }
+        res.send(result)
+}
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    console.log(xmlHttp.responseText);
+    return xmlHttp.responseText;
+}
 module.exports = { getAll }
